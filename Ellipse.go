74f10a1390ab3fd4e92e2abc4ef4a71608aa0b93
juandavid716 <jbojato@uninorte.edu.co>
2020-09-04 14:09:12 -0500

    pythagorean added

--- Ellipse.go
+++ Ellipse.go
@@ -9,7 +9,15 @@ type Init struct {
 	A, B float64
 }
 
-// GetEccentricity of Ellipse
-func (e *Init) GetEccentricity() float64 {
-	return (math.Sqrt(math.Pow(e.A, 2) - math.Pow(e.B, 2))) / e.A
+// Pythagorean theorem
+func (e *Init) Pythagorean() float64 {
+	return (math.Sqrt(math.Pow(e.A, 2) + math.Pow(e.B, 2)))
+}
+
+// GenDisplaceFn of one point
+func GenDisplaceFn(a, v0, s0 float64) func(float64) float64 {
+	fx := func(t float64) float64 {
+		return 0.5*a*math.Pow(t, 2) + v0*t + s0
+	}
+	return fx
 }
